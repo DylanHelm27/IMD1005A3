5910a8fb48762dce4d02b3a852b24dcd4c6819a6
DylanHelm27 <helm0091@algonquinlive.com>
2025-04-08 23:32:57 -0400

    Final

--- javascript/wheel.js
+++ javascript/wheel.js
@@ -2,8 +2,14 @@ let desc = document.getElementById('desc')
 
 const passions =
 {
-    p1: "test"
-
+    p1:"I first fell in love with WWE in 2009, I had a friend who watched, and he invited me to watch a show. I went through a massive fan phase from 2010-2013, but then skipped 2014, and got back into it in 2015, and have been inveted since. I love it because of the stories. The long form storytelling makes moments much more impactful when they happen. A season of a show can last for half a year, the last big wrestlingstoryline recquired 20 years of context and at minimum 2 years of attention.",
+    p2:"The Tobey Maguire Spider-Man movies were the first movies I ever connected with. My dad and I had the 60's spider-man show on when I was a kid as well, so he's a character who will always hold a special spot in my heart.",
+    p3:"Puzzles are a great relaxing hobby, I put lego in the same category. I love putting on a podcast and spending a day working on a puzzle ",
+    p4:"Stoicism is defined as 'teaches the development of self-control and fortitude as a means of overcoming destructive emotions' according to Wikipedia. This is a more recent hobby, but it's one that I enjoy, as I'm slowly reading through Meditations and marking off important sentences that stand out to me",
+    p5:"I grew up like most kids my age with a wii, my parents downloaded mario from the eshop so i grew up playing those games with them. Nowadays through humble bundels and steam sales I have a backlog of over 500 games. Maybe I'll find time someday. ",
+    p6:"The Batman (2022) is a movie that stuck with me in the same way spider-man movies did as a kid. The direction of a slow burn detective film instead of a traditional action film was the right choice, and I think this film is easily better than the dark knight.",
+    p7:"Until recently I never learned how to cook, but a new years resolution for 2023 was to learn how to. I bought HelloFresh and cooked every meal it came with, and now feel comfortable to meal prep and cook for my girlfriend. ",
+    p8:"I love coding for the same reason I love puzzles, I view pieces of the code as puzzle pieces, and bug testing (to a certain extent) are fun to hunt for and solve. This is the reason webdev was my favourite course of the year."
 }
 
 let theta = Math.PI / 4.0;
@@ -134,9 +140,9 @@ function snap_back() {
         
         `
         } else {
-            card.style.transform = `translate(-50%, -50%) rotate(${-1.0 * (wheel_theta + theta_between)}deg) scale(0.01)`;
+            card.style.transform = `translate(-50%, -50%) rotate(${-1.0 * (wheel_theta + theta_between)}deg) scale(0.1)`;
             card.style.zIndex = 1;
-        }
+     }
     });
 
     wheel_theta = wheel_theta + theta_between;
